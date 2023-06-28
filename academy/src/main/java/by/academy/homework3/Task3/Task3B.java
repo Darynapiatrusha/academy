d86899634ab6f54ba609bfdf68db11369a234c07
Darina_Petrusha <darynapiatrusha@gmail.com>
2023-06-28 23:12:12 +0300

    fix

--- academy/src/main/java/by/academy/homework3/Task3/Task3B.java
+++ academy/src/main/java/by/academy/homework3/Task3/Task3B.java
@@ -4,7 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Task3B {
-	static Pattern pattern = Pattern.compile("^(0[1-9]|[12]\\d|3[01])-(0[1-9]|1[0-2])-(([12])\\d\\d\\d)$");
+	static Pattern pattern = Pattern.compile("^(0[1-9]|[12]\\d|3[01])-(0[1-9]|1[0-2])-(([1-2]\\d{3}))$");
 
 	public static void main(String[] args) {
 		System.out.println(match("12-02-2023"));
